a198125ac0f02f4020b6f545625b20c4b3cd6bec
ferdente <64047548+ferdente@users.noreply.github.com>
2023-10-16 14:12:40 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,36 @@
 module.exports = function toReadable (number) {
-  
+    let numbero = ['zero','one', 'two','three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
+    let numbers = ['ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen']
+    let tennum = [0,0,'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety']
+
+  switch(String(number).length){
+    case 1:
+        return numbero[number]
+    case 2:
+        if (String(number)[0] == '1'){
+            return numbers[Number(String(number)[1])]
+        } else {
+            if (String(number)[1] == '0'){
+                return tennum[Number(String(number)[0])]
+            }else{
+                return tennum[Number(String(number)[0])] + " " + numbero[Number(String(number)[1])]
+            }
+        }
+    case 3:
+        if (String(number)[1] == '0' && String(number)[2] == '0'){
+            return numbero[Number(String(number)[0])] + ' hundred'
+        } else if (String(number)[1] == '1'){
+            return numbero[Number(String(number)[0])] + ' hundred ' + numbers[Number(String(number)[2])]
+        } else {
+            if (String(number)[1] == '0'){
+                return  numbero[Number(String(number)[0])] + ' hundred ' + numbero[Number(String(number)[2])]
+            }else{
+                if (String(number)[2] == '0'){
+                    return numbero[Number(String(number)[0])]+ ' hundred ' + tennum[Number(String(number)[1])]
+                } else {
+                    return numbero[Number(String(number)[0])]+ ' hundred ' + tennum[Number(String(number)[1])] + " " + numbero[Number(String(number)[2])]
+                }
+            }
+        }
+      }
 }
